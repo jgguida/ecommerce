459c2fb3272605bc9abf0d00e365ac3fe8d8ecf5
jgguida <jgguida@github.com>
2021-06-30 14:59:23 -0300

    Criando componentes frontend

---
+++ client/src/components/Navbar.js
@@ -0,0 +1,38 @@
+import React from "react";
+
+export default function Navbar() {
+  return (
+    <div>
+      <nav className="navbar navbar-expand-lg">
+        <a className="navbar-brand" href="#">
+          Minha Vendinha
+        </a>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
+          <ul className="navbar-nav ">
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Login
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+              <i class="fas fa-shopping-cart"></i>
+              </a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+  );
+}
