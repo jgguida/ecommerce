47096bffee0d0d526384fd9e3b5bde493458b8ac
jgguida <jgguida@github.com>
2021-07-05 18:41:09 -0300

    Pagina de descrição

--- client/src/components/Product.js
+++ client/src/components/Product.js
@@ -1,14 +1,15 @@
 import React from 'react'
-
+import { Link } from 'react-router-dom'
 export default function Product({prod}) {
     return (
         <div className="col-md-3 m-5 bg-white shadow rounded p-2">
             <div>
+                <Link to={`product/${prod.id}`}>
                         <img src={prod.image} className="img-fluid" alt=""></img>
                         <h1>{prod.name}</h1>
                         <h1>Rating {prod.rating}</h1>
                         <h1>Preco R${prod.price}</h1>
-                        
+                </Link>   
              </div>
              </div>
      )
--- client/src/pages/Prod_descr.js
+++ client/src/pages/Prod_descr.js
@@ -1,9 +1,11 @@
 import React from 'react'
-
-export default function Prod_descr() {
+import prod from '../Products'
+export default function Prod_descr({match}) {
+    const productid = match.params.id
     return (
         <div>
             <h1>Pagina de Descrição do produto</h1>
+            <h1>O id do poduto é {productid}</h1>
         </div>
     )
 }
