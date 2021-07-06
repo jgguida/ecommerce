7064761a0b41e5132a3ff7bb62a954502175a0e1
jgguida <jgguida@github.com>
2021-07-06 18:11:35 -0300

    Prod description page

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,10 +1,11 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Navbar() {
   return (
     <div>
       <nav className="navbar navbar-expand-lg">
-        <a className="navbar-brand" href="#">
+        <a className="navbar-brand" href="/">
           Minha Vendinha
         </a>
         <button
--- client/src/pages/Prod_descr.js
+++ client/src/pages/Prod_descr.js
@@ -1,11 +1,38 @@
-import React from 'react'
-import prod from '../Products'
-export default function Prod_descr({match}) {
-    const productid = match.params.id
-    return (
-        <div>
-            <h1>Pagina de Descrição do produto</h1>
-            <h1>O id do poduto é {productid}</h1>
+import React from "react";
+import prod from "../Products";
+export default function Prod_descr({ match }) {
+  const productid = match.params.id;
+  const product = prod.find((product) => product.id == productid);
+  return (
+    <div className="row">
+      <div className="col-md-6">
+        <div className="card p-2 m-2">
+          <h1>{product.name}</h1>
+          <img src={product.image} className="img-fluid m3 bigimg" />
+          <p>{product.description}</p>
         </div>
-    )
+      </div>
+
+      <div className="col-md-6 text-left cont">
+        <div className="m-2">
+         
+            <h1>Price: {product.price}</h1>
+          
+          <hr />
+         
+            <h1>Select Quantity</h1>
+            <br />
+
+            <select>
+              {[...Array(product.countInStock).keys()].map((x, i) => {
+                return <option value={i + 1}>{i + 1}</option>;
+              })}
+            </select>
+          
+          <hr />
+          <button className="btn btn-dark">ADD TO CART</button>
+        </div>
+      </div>
+    </div>
+  );
 }
